b1ed5bddfacd3f1befa87b505b181b773904e380
Mphammed Waleed <m.elsherbeny100@gmail.com>
2025-09-18 09:25:15 +0300

    - Finish Animation Splash Screen.
    - Navigate to Home Screen
    
    - Create Router Generator

--- lib/features/splash_screen/splash_screen.dart
+++ lib/features/splash_screen/splash_screen.dart
@@ -39,6 +39,7 @@ class _SplashScreenState extends State<SplashScreen>
     if (!mounted) return;
     Navigator.pushReplacementNamed(context, AppRouter.homeScreen);
   }
+  
 
   @override
   Widget build(BuildContext context) {
